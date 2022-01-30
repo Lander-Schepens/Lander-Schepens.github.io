74daec7e174c980b6ea0879e54a18c07b23b3552
Lander-Schepens <79144926+Lander-Schepens@users.noreply.github.com>
2022-01-30 14:26:03 +0100

    Schoonheidsfoutjes
    En partners toegevoegd, contactgegevens

--- arrayPartners.js
+++ arrayPartners.js
@@ -1,10 +1,15 @@
 export const arrayPartners = [
     {
-    "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/1009px-Playstation_logo_colour.svg.png",
-    "link" : "https://www.playstation.com",
+    "image" : "https://pbs.twimg.com/media/Eejl8edX0AA_8fA.png",
+    "link" : "https://www.azure.microsoft.com",
     },
     {
-        "image" : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/1009px-Playstation_logo_colour.svg.png",
-        "link" : "#",  
-    }
+        "image" : "https://www.designbust.com/download/1060/png/microsoft_logo_transparent512.png",
+        "link" : "https://www.microsoft.com",  
+    },
+    {
+        "image" : "https://www.lifepng.com/wp-content/uploads/2020/12/Belfius-Logo-png-hd.png",
+        "link" : "https://www.belfius.be",  
+    },
 ]
+
